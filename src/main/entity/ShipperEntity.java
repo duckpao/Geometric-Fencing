c3b42a6a94837f8ccf4d53ba52cf3ff40434691a
duckpao <nguyenducbao173@gmail.com>
2026-04-26 21:43:13 +0700

    added entity files

---
+++ src/main/entity/ShipperEntity.java
@@ -0,0 +1,32 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name= "shippers")
+public class ShipperEntity {
+     @Id
+    private int Id;
+
+    @Column(name="full_name")
+private String fullName;
+
+    @Column(name ="is_online")
+private String isOnline;
+    @Column(name="phone")
+private String phoneNumber;
+    @Column(name="vehicle_number")
+private String vehicleNumber;
+    @Column(name="created_at")
+private Date createDt;
+
+}
