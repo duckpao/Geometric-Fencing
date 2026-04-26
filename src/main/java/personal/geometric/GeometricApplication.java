2f43492d0e00e948e358df5737cb2dd8bbc46959
duckpao <nguyenducbao173@gmail.com>
2026-04-26 09:47:49 +0700

    init

---
+++ src/main/java/personal/geometric/GeometricApplication.java
@@ -0,0 +1,13 @@
+package personal.geometric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeometricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeometricApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/geometric/GeometricApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.geometric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeometricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
